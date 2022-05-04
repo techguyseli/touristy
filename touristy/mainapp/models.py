3f54ac3dfce6af35ee9627cb27d3f9a28a6facfd
techguyseli <kassouselaiman@gmail.com>
2022-05-04 11:28:53 +0000

    changes in mainapp migrations files and models

--- touristy/mainapp/models.py
+++ touristy/mainapp/models.py
@@ -1,84 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    user_name = models.CharField(max_length=50, unique=True)
-    user_hash = models.CharField(max_length=200)
-    user_salt = models.CharField(max_length=200)
-    status = models.CharField(max_length=15, default='active')
-    history_on = models.BooleanField()
-    class Meta:
-        constraints = [
-            models.CheckConstraint(check=models.Q(status__in=['active', 'banned', 'suspended']), name='status_value_in'),
-        ]
-
 class Search(models.Model):
-    search_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     search_str = models.CharField(max_length=100)
     class Meta:
-        unique_together = ['user_id', 'search_str']
-
-class Admin(models.Model):
-    admin_id = models.AutoField(primary_key=True)
-    admin_name = models.CharField(max_length=50, unique=True)
-    admin_hash = models.CharField(max_length=200)
-    admin_salt = models.CharField(max_length=200)
+        unique_together = ['user', 'search_str']
 
 class Service(models.Model):
-    service_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     latitude = models.DecimalField(max_digits=20, decimal_places=17)
     longitude = models.DecimalField(max_digits=20, decimal_places=17)
-    service_title = models.CharField(max_length=40)
-    service_type = models.CharField(max_length=20)
+    title = models.CharField(max_length=40)
+    type = models.CharField(max_length=20)
     adress = models.CharField(max_length=60)
     class Meta:
-        unique_together = ['latitude', 'longitude', 'service_title']
+        unique_together = ['latitude', 'longitude', 'title']
 
 class Image(models.Model):
-    image_id = models.AutoField(primary_key=True)
-    image_url = models.CharField(max_length=300)
-    service_id = models.ForeignKey('Service', on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    url = models.CharField(max_length=300)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
     class Meta:
-        unique_together = ['image_url', 'service_id']
+        unique_together = ['url', 'service']
 
 class Rating(models.Model):
-    rating_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    service_id = models.ForeignKey('Service', on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
     stars = models.IntegerField()
     comment_str = models.CharField(max_length=200)
     class Meta:
-        unique_together = ['user_id', 'service_id']
+        unique_together = ['user', 'service']
         constraints = [
             models.CheckConstraint(check=(models.Q(stars__gte=0) & models.Q(stars__lte=5)), name='stars_gte_0_lte_5'),
         ]
 
 class Favorite(models.Model):
-    favorite_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    service_id = models.ForeignKey('Service', on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
     add_date = models.DateField(auto_now_add=True)
     class Meta:
-        unique_together = ['user_id', 'service_id']
+        unique_together = ['user', 'service']
 
 class Plan(models.Model):
-    plan_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    plan_title = models.CharField(max_length=30)
-    plan_description = models.CharField(max_length=100)
-    plan_start = models.DateField(auto_now=False, auto_now_add=False)
-    plan_end = models.DateField(auto_now=False, auto_now_add=False)
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    start_date = models.DateField(auto_now=False, auto_now_add=False)
+    end_date = models.DateField(auto_now=False, auto_now_add=False)
     class Meta:
-        unique_together = ['plan_title', 'user_id']
+        unique_together = ['title', 'user']
 
 class Stop(models.Model):
-    stop_id = models.AutoField(primary_key=True)
-    plan_id = models.ForeignKey('Plan', on_delete=models.CASCADE)
-    service_id = models.ForeignKey('Service', on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
     stop_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
     class Meta:
-        unique_together = ['plan_id', 'service_id', 'stop_datetime']
+        unique_together = ['plan', 'service', 'stop_datetime']
