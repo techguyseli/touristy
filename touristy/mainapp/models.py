6cb829d9f1b96f6cbdda58feacdb5989ee4d6ff7
techguyseli <kassouselaiman@gmail.com>
2022-05-04 15:51:20 +0000

    added authenticate app, main appbase template and static files and changed models

--- touristy/mainapp/models.py
+++ touristy/mainapp/models.py
@@ -4,14 +4,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Search(models.Model):
-    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     search_str = models.CharField(max_length=100)
     class Meta:
-        unique_together = ['user', 'search_str']
+        unique_together = ['user' , 'search_str']
 
 class Service(models.Model):
-    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     latitude = models.DecimalField(max_digits=20, decimal_places=17)
     longitude = models.DecimalField(max_digits=20, decimal_places=17)
@@ -22,14 +20,12 @@ class Service(models.Model):
         unique_together = ['latitude', 'longitude', 'title']
 
 class Image(models.Model):
-    id = models.AutoField(primary_key=True)
     url = models.CharField(max_length=300)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     class Meta:
         unique_together = ['url', 'service']
 
 class Rating(models.Model):
-    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     stars = models.IntegerField()
@@ -41,7 +37,6 @@ class Rating(models.Model):
         ]
 
 class Favorite(models.Model):
-    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     add_date = models.DateField(auto_now_add=True)
@@ -49,7 +44,6 @@ class Favorite(models.Model):
         unique_together = ['user', 'service']
 
 class Plan(models.Model):
-    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
@@ -59,7 +53,6 @@ class Plan(models.Model):
         unique_together = ['title', 'user']
 
 class Stop(models.Model):
-    id = models.AutoField(primary_key=True)
     plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     stop_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
--- touristy/touristy/settings.py
+++ touristy/touristy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'authentication',
     'mainapp',
     'django.contrib.admin',
     'django.contrib.auth',
