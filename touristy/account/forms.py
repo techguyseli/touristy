dd4f293064975095193be412564d0a13582d659c
techguyseli <kassouselaiman@gmail.com>
2022-05-05 13:59:38 +0000

    fixed register and login templates' layout

--- touristy/account/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class RegisterForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=70)
-    password = forms.CharField(label='Password', max_length=70)
-    password_confirmation = forms.CharField(label='Repeat password', max_length=70)
