48a098a8e5139f0e94d032bf4d329d4475450fb8
MedClch <chlouchi.med@gmail.com>
2022-05-06 13:04:17 +0000

    taille username modifiee!

--- touristy/account/forms.py
+++ touristy/account/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class RegisterUserForm(forms.Form):
     csrfmiddlewaretoken = forms.CharField(required=True)
-    username = forms.CharField(min_length=8, max_length = 40, required=True)
+    username = forms.CharField(min_length=5, max_length = 40, required=True)
     password = forms.CharField(min_length=8, max_length = 40, required=True)
     password_repeat = forms.CharField(min_length=8, max_length = 40, required=True)
